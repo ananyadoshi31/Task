b5cb5b7d86af92a18b420442160341be55772344
Ananya Doshi <doshiananya2002@gmail.com>
2025-03-25 23:49:14 -0400

    added comments

--- src/App.js
+++ src/App.js
@@ -6,17 +6,27 @@ import Users from "./components/Users";
 import UserDetails from "./components/UserDetails";
 
 function App() {
+  // state for storing users
   const [users, setUsers] = useState([]);
+  // state for storing filtered users
   const [filteredUsers, setFilteredUsers] = useState([]);
+  // state for storing particular user to view further details
   const [selectedUser, setSelectedUser] = useState(null);
+
+  // loading and error handling
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+
+  // state for search input
   const [search, setSearch] = useState("");
+
+  // state for sorting
   const [sortField, setSortField] = useState("");
   const [sortOrder, setSortOrder] = useState("asc");
 
-  const detailRef = useRef(null); // create a ref
+  const detailRef = useRef(null); //reference for scroll to user details whenevr selected
 
+  // fetches all users from API on initial render request
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/users")
@@ -34,14 +44,14 @@ function App() {
   useEffect(() => {
     let updated = [...users];
 
-    // Filter
+    // implementing filter (based on email and name) functionality
     updated = updated.filter(
       (user) =>
         user.name.toLowerCase().includes(search.toLowerCase()) ||
         user.email.toLowerCase().includes(search.toLowerCase())
     );
 
-    // Sort
+    // implementing sort functionality if that field is selected
     if (sortField) {
       updated.sort((a, b) => {
         if (sortOrder === "asc") {
@@ -55,6 +65,7 @@ function App() {
     setFilteredUsers(updated);
   }, [search, sortField, sortOrder, users]);
 
+  // loading and error handling
   if (loading) return <p>Loading users...</p>;
   if (error) return <p>Error fetching users.</p>;
 
@@ -65,7 +76,7 @@ function App() {
       <div className="controls">
         <input
           type="text"
-          placeholder="Search by name or email..."
+          placeholder="Search by name or email.."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
@@ -94,12 +105,14 @@ function App() {
               setSelectedUser(user);
               setTimeout(() => {
                 detailRef.current?.scrollIntoView({ behavior: "smooth" });
-              }, 100); // wait briefly for component to render
+              }, 100);
             }}
           />
         ))}
       </div>
 
+      {/* 
+        diplays details of user if selected further */}
       {selectedUser && (
         <div ref={detailRef}>
           <UserDetails user={selectedUser} />
